4462f52e81f525809caf62d34281f265bb27e0c2
Oliwia Paliwoda <olipal001@pbs.edu.pl>
2026-05-02 18:22:21 +0200

    ts template init

---
+++ frontend/src/views/Homepage.tsx
@@ -0,0 +1,13 @@
+import "./styles/Homepage.scss"
+
+function Homepage() {
+    return (
+        <div className="main-container">
+            <div className="header">test</div>
+            <div className="content"></div>
+            <div className="footer">test</div>
+        </div>
+    )
+}
+
+export default Homepage;
